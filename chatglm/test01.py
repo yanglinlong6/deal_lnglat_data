14e69c83c00a8f8d9f4afd73be05a9fa82ff4537
yanglinlong6 <592761523@qq.com>
2024-02-26 23:19:06 +0800

    添加智谱

---
+++ chatglm/test01.py
@@ -0,0 +1,22 @@
+from zhipuai import ZhipuAI
+
+client = ZhipuAI(
+    api_key="2544ea141595d420ca10f891833096ac.deW5ICKFVh4JivTN"
+)  # 填写您自己的APIKey
+response = client.chat.completions.create(
+    model="glm-4",  # 填写需要调用的模型名称
+    messages=[
+        {
+            "role": "user",
+            "content": "作为一名营销专家，请为我的产品创作一个吸引人的slogan",
+        },
+        {
+            "role": "assistant",
+            "content": "当然，为了创作一个吸引人的slogan，请告诉我一些关于您产品的信息",
+        },
+        {"role": "user", "content": "智谱AI开放平台"},
+        {"role": "assistant", "content": "智启未来，谱绘无限一智谱AI，让创新触手可及!"},
+        {"role": "user", "content": "创造一个更精准、吸引人的slogan"},
+    ],
+)
+print(response.choices[0].message)
